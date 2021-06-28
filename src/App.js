587a65c5d643d3f678a94dfad8b92e5d6142506c
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-06-28 16:28:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className="App">testpage</div>;
+}
+
+export default App;
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,14 @@
+import * as types from '../actions/types';
+
+const initialState = {
+  user: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
