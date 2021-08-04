034eeefd8182d6f9bbb1ef6585313d55857c4560
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-08-04 17:42:14 +0300

    add what are you waiting for

--- src/pages/TourDetails/index.js
+++ src/pages/TourDetails/index.js
@@ -6,17 +6,16 @@ import * as endpoints from "../../configs/endpointConfig";
 import Loading from "../../components/Loading";
 
 import { EnvironmentOutlined, FieldTimeOutlined } from "@ant-design/icons";
-import { Col, Divider, Row } from "antd";
+import { Avatar } from "antd";
 
 function TourDetails() {
   const dispatch = useDispatch();
   const tour = useSelector((state) => state.tours.tour);
-  const tourCover =
-    tour && `${endpoints.BACKEND_URL}/img/tours/${tour.imageCover}`;
+  const backendImg = `${endpoints.BACKEND_URL}/img/tours/`;
   const routeParams = useParams();
+  const imgurl = `${backendImg}${tour?.images[0]}`;
 
   useEffect(() => {
-    console.log(routeParams.id);
     dispatch(getTour(routeParams.id));
   }, [routeParams]);
   return !tour ? (
@@ -28,7 +27,7 @@ function TourDetails() {
         style={{
           backgroundImage: `linear-gradient(to right bottom,
             hsla(111, 55%, 64%, 0.8),
-            hsla(160, 64%, 43%, 0.8)), url('${tourCover}')`,
+            hsla(160, 64%, 43%, 0.8)), url('${backendImg}${tour.imageCover}')`,
         }}
       >
         <div className="header__content">
@@ -40,7 +39,7 @@ function TourDetails() {
           <div style={{ display: "flex" }}>
             <div>
               <FieldTimeOutlined className="header__heading__icon" />
-              <span>{tour.duration}</span>
+              <span>{tour.duration} DAYS</span>
             </div>
             <div style={{ marginLeft: "2rem" }}>
               <EnvironmentOutlined className="header__heading__icon" />
@@ -49,6 +48,47 @@ function TourDetails() {
           </div>
         </div>
       </div>
+      <section className="waitSection">
+        <div className="Whatwaiting">
+          <Avatar.Group
+            maxCount={3}
+            size={125}
+            maxStyle={{
+              color: "#f56a00",
+              backgroundColor: "#fde3cf",
+            }}
+          >
+            <Avatar
+              src={backendImg.concat(tour.images[0])}
+              style={{
+                marginLeft: "35%",
+              }}
+            />
+            <Avatar
+              src={backendImg.concat(tour.images[1])}
+              style={{
+                marginLeft: "-50%",
+              }}
+            />
+            <Avatar
+              src={backendImg.concat(tour.images[2])}
+              style={{
+                marginLeft: "-50%",
+              }}
+            />
+          </Avatar.Group>
+          <div style={{ marginLeft: "-5rem", marginRight: "5rem" }}>
+            <h1 className="title">what are you waiting for?</h1>
+            <p>
+              {tour.duration} days, 1 advanture, infinite memories, make it
+              yours today!
+            </p>
+          </div>
+          <a href="#" className="button button--green">
+            LOGIN TO BOOK TOUR
+          </a>
+        </div>
+      </section>
     </div>
   );
 }
