513aea3b94b3b3ca216a1cb21ff757f0afb2173c
Muhannad <muhannad35990@gmail.com>
2021-08-07 17:37:45 +0300

    finsish map with markers and hover description

--- src/components/mapBox/MapBox.js
+++ src/components/mapBox/MapBox.js
@@ -5,11 +5,11 @@ import { useSelector } from "react-redux";
 mapboxgl.accessToken =
   "pk.eyJ1IjoibXVoYW5uYWQzNTk5MCIsImEiOiJja3J5eGJ5aGsxNHB2Mm9uODUzejEwanAxIn0.rzqtpU0RJv8KrLpRCp-ddw";
 function MapBox() {
-  const mapContainer = useRef(null);
+  let mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [zoom, setZoom] = useState(4);
 
   const tour = useSelector((state) => state.tours.tour);
   useEffect(() => {
@@ -20,7 +20,23 @@ function MapBox() {
       center: [lng, lat],
       zoom: zoom,
     });
+    const bounds = new mapboxgl.LngLatBounds();
+    map.current.on("load", () => {
+      tour.locations.forEach((loc) => {
+        const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+          loc.description
+        );
+        new mapboxgl.Marker()
+          .setLngLat(loc.coordinates)
+          .setPopup(popup)
+          .addTo(map.current);
+
+        bounds.extend(loc.coordinates);
+      });
+      map.current.fitBounds(bounds);
+    });
   });
+
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
     map.current.on("move", () => {
@@ -38,11 +54,11 @@ function MapBox() {
   });
 
   return (
-    <div style={{ position: "relative" }}>
-      <div className="sidebar">
+    <div>
+      {/* <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} />
+      </div> */}
+      <div ref={(el) => (mapContainer.current = el)} />
     </div>
   );
 }
